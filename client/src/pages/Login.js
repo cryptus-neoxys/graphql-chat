4714fa4b4f92aed2f9ffd5a2e5bf9b3e4bf13cec
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-10 02:23:10 +0530

    Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import { gql, useLazyQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const LOGIN_USER = gql`
+  query login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      username
+      email
+      createdAt
+      token
+    }
+  }
+`;
+
+export default function Register(props) {
+  const [variables, setVariables] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
+    onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
+    onCompleted: (data) => {
+      localStorage.setItem("token", data.login.token);
+      props.history.push("/");
+    },
+  });
+
+  const submitLoginForm = async (e) => {
+    e.preventDefault();
+    setErrors({});
+    loginUser({ variables });
+  };
+
+  return (
+    <Row className="bg-white py-5 justify-content-center">
+      <Col sm={8} md={6} lg={4}>
+        <h1 className="text-center">Login</h1>
+        <Form onSubmit={submitLoginForm}>
+          <Form.Group>
+            <Form.Label className={errors.username && "text-danger"}>
+              {errors.username ?? "Username"}
+            </Form.Label>
+            <Form.Control
+              className={errors.username && "is-invalid"}
+              type="text"
+              value={variables.username}
+              onChange={(e) =>
+                setVariables({ ...variables, username: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className={errors.password && "text-danger"}>
+              {errors.password ?? "Password"}
+            </Form.Label>
+            <Form.Control
+              type="password"
+              className={errors.password && "is-invalid"}
+              value={variables.password}
+              onChange={(e) =>
+                setVariables({ ...variables, password: e.target.value })
+              }
+            />
+          </Form.Group>
+          <div className="text-center">
+            <Button variant="success" type="submit" disabled={loading}>
+              {loading ? "Loading ..." : "Login"}
+            </Button>
+            <br />
+            <small>
+              Don't have an Account? <Link to="/register">Register</Link>
+            </small>
+          </div>
+        </Form>
+      </Col>
+    </Row>
+  );
+}
