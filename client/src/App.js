4f17e1128cafd09f3252132410729d7c6141cd84
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-10 02:23:26 +0530

    App with react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,27 @@
-import { useState } from "react";
-import { Container, Row, Col, Button, Form, Card } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import ApolloProvider from "./ApolloProvider";
 
 import "./App.scss";
 
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
-  const [variables, setVariables] = useState({
-    email: "",
-    username: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const submitRegisterForm = async (e) => {
-    e.preventDefault();
-  };
-
   return (
-    <Container className="pt-5">
-      <Row className="bg-white py-5 justify-content-center">
-        <Col sm={8} md={6} lg={4}>
-          <h1 className="text-center">Register</h1>
-          <Form onSubmit={submitRegisterForm}>
-            <Form.Group>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" />
-            </Form.Group>
-            <div className="text-center">
-              <Button variant="success" type="submit">
-                Register
-              </Button>
-            </div>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <ApolloProvider>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
