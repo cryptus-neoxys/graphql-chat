5b13b8d768636cb681c095a0a9dcae71179f0f48
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-13 03:31:14 +0530

    err condition ommited

--- util/contextMiddleware.js
+++ util/contextMiddleware.js
@@ -5,10 +5,7 @@ const { JWT_SECRET } = require("../config/env.json");
 module.exports = (context) => {
   if (context.req && context.req.headers.authorization) {
     const token = context.req.headers.authorization.split("Bearer ")[1];
-    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        // throw new AuthenticationError("Unauthenticated");
-      }
+    jwt.verify(token, JWT_SECRET, (_, decodedToken) => {
       context.user = decodedToken;
     });
   }
