7b580c235ca52f950e1781ffcf367d644ea820f3
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-14 03:18:47 +0530

    Home nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Button, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+import { useAuthDispatch } from "../context/auth";
+
+export default function Home({ history }) {
+  const dispatch = useAuthDispatch();
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Row className="bg-white justify-content-around">
+      <Link to="/login">
+        <Button variant="link">Login</Button>
+      </Link>
+      <Link to="/register">
+        <Button variant="link">Register</Button>
+      </Link>
+      <Button variant="link" onClick={logout}>
+        Logout
+      </Button>
+    </Row>
   );
 }
