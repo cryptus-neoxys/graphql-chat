21c32b7b0f4e95e00bea492063ce415b3f9de4fb
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-19 03:56:01 +0530

    Message with time tooltip

--- client/src/pages/home/Message.js
+++ client/src/pages/home/Message.js
@@ -1,25 +1,38 @@
 import React from "react";
 import classNames from "classnames";
+import moment from "moment";
 
 import { useAuthState } from "../../context/auth";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export default function Message({ message }) {
   const { user } = useAuthState();
   const sent = message.from === user.username;
   const recieved = !sent;
   return (
-    <div
-      className={classNames("d-flex py-3", {
-        "ml-auto": sent,
-        "mr-auto": recieved,
-      })}>
+    <OverlayTrigger
+      placement={recieved ? "right" : "left"}
+      overlay={
+        <Tooltip>
+          {moment(message.createdAt).format("MMMM DD, YYYY @ h:mm a")}
+        </Tooltip>
+      }
+      transition={false}>
       <div
-        className={classNames("py-2 px-3 rounded-pill", {
-          "bg-primary": sent,
-          "bg-secondary": recieved,
+        className={classNames("d-flex py-3", {
+          "ml-auto": sent,
+          "mr-auto": recieved,
         })}>
-        <p className={classNames({ "text-white": sent })}>{message.content}</p>
+        <div
+          className={classNames("py-2 px-3 rounded-pill", {
+            "bg-primary": sent,
+            "bg-secondary": recieved,
+          })}>
+          <p className={classNames({ "text-white": sent })}>
+            {message.content}
+          </p>
+        </div>
       </div>
-    </div>
+    </OverlayTrigger>
   );
 }
