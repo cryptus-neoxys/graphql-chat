5a6acad4598d0db4ce5ebf5a42b8c3717a92ad4f
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-19 02:15:37 +0530

    User cmp

---
+++ client/src/pages/home/Users.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Col, Image } from "react-bootstrap";
+import classNames from "classnames";
+
+import { useMessageDispatch, useMessageState } from "../../context/message";
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      username
+      email
+      createdAt
+      imageUrl
+      latestMessage {
+        uuid
+        content
+        from
+        to
+        createdAt
+      }
+    }
+  }
+`;
+
+function Users({ setSelectedUser, selectedUser }) {
+  const dispatch = useMessageDispatch();
+  const { users } = useMessageState();
+
+  const { loading } = useQuery(GET_USERS, {
+    onCompleted: (data) =>
+      dispatch({ type: "SET_USERS", payload: data.getUsers }),
+    onError: (err) => console.log(err),
+  });
+
+  let usersMarkup;
+  if (!users || loading) {
+    usersMarkup = <p>loading ...</p>;
+  } else if (users.length === 0) {
+    usersMarkup = <p>No users have joined yet </p>;
+  } else if (users.length > 1) {
+    usersMarkup = users.map((user) => {
+      const selected = selectedUser === user.username;
+      return (
+        <div
+          role="button"
+          className={classNames("user-div d-flex p-3", {
+            "bg-white": selected,
+          })}
+          key={user.username}
+          onClick={() => setSelectedUser(user.username)}>
+          <Image
+            src={user.imageUrl}
+            roundedCircle
+            className="mr-2"
+            style={{ width: 50, height: 50, objectFit: "cover" }}
+          />
+          <div>
+            <p className="text-success">{user.username}</p>
+            <p className="font-weight-light">
+              {user.latestMessage
+                ? user.latestMessage.content
+                : "You are now connected"}
+            </p>
+          </div>
+        </div>
+      );
+    });
+  }
+  return (
+    <Col xs={4} className="p-0 bg-secondary">
+      {usersMarkup}
+    </Col>
+  );
+}
+
+export default Users;
