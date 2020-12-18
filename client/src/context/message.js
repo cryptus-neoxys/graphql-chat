65ac6402b7c1104994cbf3ae43bfca8892504994
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-19 03:01:14 +0530

    message context done

--- client/src/context/message.js
+++ client/src/context/message.js
@@ -4,11 +4,34 @@ const MessageStateContext = createContext();
 const MessageDispatchContext = createContext();
 
 const messageReducer = (state, action) => {
+  let updatedUsers;
   switch (action.type) {
     case "SET_USERS":
       return {
         ...state,
         users: action.payload,
+      };
+    case "SET_USER_MESSAGES":
+      const { username, messages } = action.payload;
+      updatedUsers = [...state.users];
+
+      const userIndex = updatedUsers.findIndex((u) => u.username === username);
+
+      updatedUsers[userIndex] = { ...updatedUsers[userIndex], messages };
+
+      return {
+        ...state,
+        users: updatedUsers,
+      };
+    case "SET_SELECTED_USER":
+      updatedUsers = state.users.map((user) => ({
+        ...user,
+        selected: user.username === action.payload,
+      }));
+
+      return {
+        ...state,
+        users: updatedUsers,
       };
     default:
       throw new Error(`Unknown action type: ${action.type}`);
