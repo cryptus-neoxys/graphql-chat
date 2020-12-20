22c023bd34a138a94d01bf0024eee82c8dc0234a
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-21 03:13:49 +0530

    subscriptions secured with JWT Bearer

--- util/contextMiddleware.js
+++ util/contextMiddleware.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
+const { PubSub } = require("apollo-server");
+
+const pubsub = new PubSub();
 
 const { JWT_SECRET } = require("../config/env.json");
 
 module.exports = (context) => {
+  let token;
   if (context.req && context.req.headers.authorization) {
-    const token = context.req.headers.authorization.split("Bearer ")[1];
+    token = context.req.headers.authorization.split("Bearer ")[1];
+  } else if (context.connection && context.connection.context.Authorization) {
+    token = context.connection.context.Authorization.split("Bearer ")[1];
+  }
+
+  context.pubsub = pubsub;
+
+  if (token) {
     jwt.verify(token, JWT_SECRET, (_, decodedToken) => {
       context.user = decodedToken;
     });
